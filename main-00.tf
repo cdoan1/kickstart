1a0bb96e014c8ed32d2fd7d4297501cc12d436fa
Chris Doan <cdoan@us.ibm.com>
2018-09-16 11:09:20 -0500

    add firewall rules

--- main-00.tf
+++ main-00.tf
@@ -1,7 +1,9 @@
 ## Setup needed variables
 
 variable "master_count" {}
-variable "node_count" {}
+variable "va_count" {}
+variable "manage_count" {}
+variable "worker_count" {}
 variable "proxy_count" {}
 variable "vipmaster_count" {}
 variable "vipproxy_count" {}
@@ -62,6 +64,88 @@ resource "softlayer_virtual_guest" "master" {
   private_network_only = false
   cpu                  = "${var.master_vcpu}"
   ram                  = "${var.master_ram}"
+  disks                = [100]
+  local_disk           = false
+
+  provisioner "local-exec" {
+    command = "scp ${path.module}/scripts/setup-docker.sh.tpl root@${self.ipv4_address_private}:/tmp"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} 'chmod 755 /tmp/setup-docker.sh.tpl ; ls -al /tmp/setup-docker.sh.tpl'"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} '/tmp/setup-docker.sh.tpl'"
+  }
+
+  # update authorized_keys
+  provisioner "local-exec" {
+    command = "scp ${path.module}/scripts/id_rsa.pub root@${self.ipv4_address_private}:/root/.ssh"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} 'cd /root/.ssh ; cat id_rsa.pub >> authorized_keys'"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} sed -i 's/127.0.1.1/${self.ipv4_address_private}/g' /etc/hosts"
+  }
+}
+
+resource "softlayer_virtual_guest" "va" {
+  count                = "${var.va_count}"
+  image                = "${var.image_name}"
+  name                 = "${var.prefix}-va-${format("%d",count.index + 1)}"
+  domain               = "${var.dc}.${var.domain}"
+  ssh_keys             = ["922553"]
+  image                = "${var.image_name}"
+  region               = "${var.dc}"
+  hourly_billing       = true
+  private_network_only = false
+  cpu                  = "${var.master_vcpu}"
+  ram                  = "${var.master_ram}"
+  disks                = [100]
+  local_disk           = false
+
+  provisioner "local-exec" {
+    command = "scp ${path.module}/scripts/setup-docker.sh.tpl root@${self.ipv4_address_private}:/tmp"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} 'chmod 755 /tmp/setup-docker.sh.tpl ; ls -al /tmp/setup-docker.sh.tpl'"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} '/tmp/setup-docker.sh.tpl'"
+  }
+
+  # update authorized_keys
+  provisioner "local-exec" {
+    command = "scp ${path.module}/scripts/id_rsa.pub root@${self.ipv4_address_private}:/root/.ssh"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} 'cd /root/.ssh ; cat id_rsa.pub >> authorized_keys'"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh root@${self.ipv4_address_private} sed -i 's/127.0.1.1/${self.ipv4_address_private}/g' /etc/hosts"
+  }
+}
+
+resource "softlayer_virtual_guest" "manage" {
+  count                = "${var.manage_count}"
+  image                = "${var.image_name}"
+  name                 = "${var.prefix}-manage-${format("%d",count.index + 1)}"
+  domain               = "${var.dc}.${var.domain}"
+  ssh_keys             = ["922553"]
+  image                = "${var.image_name}"
+  region               = "${var.dc}"
+  hourly_billing       = true
+  private_network_only = false
+  cpu                  = "${var.master_vcpu}"
+  ram                  = "${var.master_ram}"
   disks                = [100,200]
   local_disk           = false
 
@@ -70,7 +154,7 @@ resource "softlayer_virtual_guest" "master" {
   }
 
   provisioner "local-exec" {
-    command = "ssh root@${self.ipv4_address_private} 'chmod 755 /tmp/setup-docker.sh.tpl'"
+    command = "ssh root@${self.ipv4_address_private} 'chmod 755 /tmp/setup-docker.sh.tpl ; ls -al /tmp/setup-docker.sh.tpl'"
   }
 
   provisioner "local-exec" {
@@ -92,7 +176,7 @@ resource "softlayer_virtual_guest" "master" {
 }
 
 resource "softlayer_virtual_guest" "worker" {
-  count                = "${var.node_count}"
+  count                = "${var.worker_count}"
   name                 = "${var.prefix}-worker-${format("%d",count.index + 1)}"
   image                = "${var.image_name}"
   domain               = "${var.dc}.${var.domain}"
@@ -102,14 +186,12 @@ resource "softlayer_virtual_guest" "worker" {
   private_network_only = false
   cpu                  = "${var.worker_vcpu}"
   ram                  = "${var.worker_ram}"
-  disks                = [100,200]
+  disks                = [100]
   local_disk           = false
 
   provisioner "local-exec" {
     command = "scp ${path.module}/scripts/setup-docker.sh.tpl root@${self.ipv4_address_private}:/tmp"
   }
-
-
 
   provisioner "local-exec" {
     command = "ssh root@${self.ipv4_address_private} 'chmod 755 /tmp/setup-docker.sh.tpl ; ls -al /tmp/setup-docker.sh.tpl'"
--- scripts/setup-docker.sh.tpl
+++ scripts/setup-docker.sh.tpl
@@ -27,5 +27,29 @@ systemctl enable docker.service
 systemctl restart docker.service
 systemctl status docker.service
 
-iptables -A INPUT -i eth1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -i eth1 -j DROP
+## First, set default policy and flush existing rules. 
+## No point really to set default policy to open other than sometimes it helps when
+## working remotely and you don't want to lose your ssh session if there is a typo.
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+## Drop invalid packets
+iptables -A INPUT -m state --state INVALID -j DROP
+
+## Allow Established and Related communications in/out
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i eth0 -j ACCEPT
+iptables -A OUTPUT -i eth0 -j ACCEPT
+
+# iptables -A INPUT -i eth1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# iptables -A INPUT -i eth1 -j DROP
+
+## Default Policy
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+# iptables -P OUTPUT DROP
